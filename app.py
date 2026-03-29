8bd6ef408a7a9a18c6bc8ef97ac2f07767c2e9fc
ThanhTuyen0302 <doancaothanhtuyen0302@gmail.com>
2026-03-29 11:48:45 +0700

    test

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import numpy as np
 import joblib
+import pandas as pd
+from savedata import save_data
 
 # load model
 data = joblib.load("heart_model.h5")
@@ -14,11 +16,21 @@ st.write("Nhập thông tin của bạn để dự đoán nguy cơ mắc bệnh 
 # input
 age = st.number_input("Tuổi", 20, 100, 50)
 sex = st.selectbox("Giới tính", ["Nam", "Nữ"])
+dataset = "Cleveland"
 cp = st.selectbox("Loại đau ngực", ["Không đau ngực", "Đau ngực không điển hình", "Đau ngực điển hình", "Không đau ngực"])
 trestbps = st.number_input("Huyết áp", 80, 200, 120)
 chol = st.number_input("Cholesterol", 100, 400, 200)
+fbs = st.selectbox("Đường huyết lúc đói > 120 mg/dl", ["Có", "Không"])
+restecg = st.selectbox("Kết quả điện tâm đồ", ["Bình thường", "Có bất thường", "Có sóng Q"])
 thalach = st.number_input("Nhịp tim tối đa", 60, 220, 150)
+exang = st.selectbox("Có đau ngực khi tập thể dục không?", ["Có", "Không"])
 oldpeak = st.number_input("Giảm ST", 0.0, 10.0, 1.0)
+slope = st.selectbox("Độ dốc của đoạn ST", ["Lên", "Bằng phẳng", "Xuống"])
+ca = st.number_input("Số lượng mạch chính", 0, 3, 0)
+thal = st.selectbox("Thalassemia", ["Bình thường", "Có bất thường cố định", "Có bất thường đảo ngược"])
+num = 0 if st.button("Lưu dữ liệu") else None 
+if num is not None:
+    save_data(age, sex, dataset, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, num)
 
 # encode
 sex = 1 if sex == "Nam" else 0
---
+++ savedata.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import csv
+def save_data(age,sex,dataset,cp,trestbps,chol,fbs,restecg,thalch,exang,oldpeak,slope,ca,thal,num):
+    df = pd.read_csv("userinput.csv")
+    id = "U"+ str(len(df) + 1)
+    data = {
+        "id": [id],
+        "age": [age],
+        "sex": [sex],
+        "dataset": [dataset],
+        "cp": [cp],
+        "trestbps": [trestbps],
+        "chol": [chol],
+        "fbs": [fbs],
+        "restecg": [restecg],
+        "thalch": [thalch],
+        "exang": [exang],
+        "oldpeak": [oldpeak],
+        "slope": [slope],
+        "ca": [ca],
+        "thal": [thal],
+        "num": [num]
+    }
+    df = pd.DataFrame(data)
+    df.to_csv("userinput.csv", mode="a", header=False, index=False)
+    print("Data saved successfully.")
